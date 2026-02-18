0593d856bc98af7e1f5d23727679f137f50484f7
Roman <dev@rman.dev>
2026-02-18 10:20:09 +0000

    wip

--- packages/archiver/tests/helpers/index.js
+++ packages/archiver/tests/helpers/index.js
@@ -2,17 +2,6 @@ import crypto from "crypto";
 import { readFileSync, WriteStream } from "fs";
 import { inherits } from "util";
 import { Stream } from "stream";
-import { Readable, Writable } from "readable-stream";
-
-export function adjustDateByOffset(d, offset) {
-  d = d instanceof Date ? d : new Date();
-  if (offset >= 1) {
-    d.setMinutes(d.getMinutes() - offset);
-  } else {
-    d.setMinutes(d.getMinutes() + Math.abs(offset));
-  }
-  return d;
-}
 
 export function binaryBuffer(n) {
   var buffer = Buffer.alloc(n);
@@ -21,25 +10,6 @@ export function binaryBuffer(n) {
   }
   return buffer;
 }
-
-function BinaryStream(size, options) {
-  Readable.call(this, options);
-  var buf = Buffer.alloc(size);
-  for (var i = 0; i < size; i++) {
-    buf.writeUInt8(i & 255, i);
-  }
-  this.push(buf);
-  this.push(null);
-}
-inherits(BinaryStream, Readable);
-BinaryStream.prototype._read = function (size) {};
-function DeadEndStream(options) {
-  Writable.call(this, options);
-}
-inherits(DeadEndStream, Writable);
-DeadEndStream.prototype._write = function (chuck, encoding, callback) {
-  callback();
-};
 
 export function readJSON(filepath) {
   var contents;
@@ -72,7 +42,4 @@ WriteHashStream.prototype.write = function (chunk) {
   return WriteStream.prototype.write.call(this, chunk);
 };
 
-export { BinaryStream };
-export { DeadEndStream };
-export { UnBufferedStream };
-export { WriteHashStream };
+export { UnBufferedStream, WriteHashStream };
