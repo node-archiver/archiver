60ca43768dc1f522ac0cd6d04c20b1d63b19bd38
Roman <dev@rman.dev>
2026-02-19 14:12:21 +0000

    Remove events-universal

--- packages/tar-stream/src/streamx.ts
+++ packages/tar-stream/src/streamx.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from "events-universal";
+import { EventEmitter } from "node:events";
 const STREAM_DESTROYED = new Error("Stream was destroyed");
 const PREMATURE_CLOSE = new Error("Premature close");
 
