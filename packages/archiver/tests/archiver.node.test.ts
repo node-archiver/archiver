3371d07bcf7b85d8001795824d58c03d2d2aceda
Roman <dev@rman.dev>
2026-03-11 14:00:13 +0000

    wip

--- packages/archiver/tests/archiver.node.test.ts
+++ packages/archiver/tests/archiver.node.test.ts
@@ -11,6 +11,8 @@ import {
   mkdirSync,
 } from "node:fs";
 import { Readable } from "node:stream";
+import { binaryBuffer, readJSON } from "./helpers/index.ts";
+import { sanitizePath,dateify } from "@archiver/archiver/utils";
 
 import { JsonArchive } from "@archiver/archiver";
 function normalizeEntryData(data, stats?) {
@@ -74,8 +76,6 @@ function normalizeEntryData(data, stats?) {
   }
   return normalizedData;
 }
-import { binaryBuffer, readJSON } from "./helpers/index.ts";
-import { sanitizePath } from "@archiver/archiver/utils";
 
 const testBuffer = binaryBuffer(1024 * 16);
 const testDate = new Date("Jan 03 2013 14:26:38 GMT");
