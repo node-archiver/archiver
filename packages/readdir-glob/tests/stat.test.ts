293dc798815bff37995f92c5cd1fcf3713662cb9
Roman <dev@rman.dev>
2026-03-11 17:52:34 +0000

    stupid fs

--- packages/readdir-glob/tests/stat.test.ts
+++ packages/readdir-glob/tests/stat.test.ts
@@ -10,7 +10,28 @@ describe("stat", () => {
     const matches: string[] = [];
     g.on("match", (m) => {
       matches.push(m.relative);
-      expect(m.stat).toBeInstanceOf(Stats);
+      expect(m.stat).not.toBeInstanceOf(Stats);
+      expect(m.stat).toHaveProperty("isFile");
+      expect(m.stat).toHaveProperty("isDirectory");
+      expect(m.stat).toHaveProperty("isSymbolicLink");
+      expect(m.stat).toHaveProperty("isBlockDevice");
+      expect(m.stat).toHaveProperty("isCharacterDevice");
+      expect(m.stat).toHaveProperty("isFIFO");
+      expect(m.stat).toHaveProperty("isSocket");
+      expect(m.stat).toHaveProperty("dev");
+      expect(m.stat).toHaveProperty("ino");
+      expect(m.stat).toHaveProperty("mode");
+      expect(m.stat).toHaveProperty("nlink");
+      expect(m.stat).toHaveProperty("uid");
+      expect(m.stat).toHaveProperty("gid");
+      expect(m.stat).toHaveProperty("rdev");
+      expect(m.stat).toHaveProperty("size");
+      expect(m.stat).toHaveProperty("blksize");
+      expect(m.stat).toHaveProperty("blocks");
+      expect(m.stat).toHaveProperty("atimeMs");
+      expect(m.stat).toHaveProperty("mtimeMs");
+      expect(m.stat).toHaveProperty("ctimeMs");
+      expect(m.stat).toHaveProperty("birthtimeMs");
     });
     g.on("end", () => {
       expect(matches.toSorted()).toEqual(
