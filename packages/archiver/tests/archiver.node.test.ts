286fb28d6968e208edc92e05f1f46229c776c1e6
Roman <dev@rman.dev>
2026-03-11 13:53:47 +0000

    wip

--- packages/archiver/tests/archiver.node.test.ts
+++ packages/archiver/tests/archiver.node.test.ts
@@ -12,9 +12,9 @@ import {
 } from "node:fs";
 import { Readable } from "node:stream";
 
-import { JsonArchive } from "../src";
-import { normalizeEntryData } from "../src/lib/core";
-import { binaryBuffer, readJSON } from "./helpers";
+import { JsonArchive } from "../src/index.ts";
+import { normalizeEntryData } from "../src/lib/core.ts";
+import { binaryBuffer, readJSON } from "./helpers/index.ts";
 
 const testBuffer = binaryBuffer(1024 * 16);
 const testDate = new Date("Jan 03 2013 14:26:38 GMT");
