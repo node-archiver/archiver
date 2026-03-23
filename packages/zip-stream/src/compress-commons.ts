ebcc1f3052f35d90aa06d083e47bf376e03afeea
Roman <dev@rman.dev>
2026-03-23 20:24:15 +0000

    chore: remove unused exports (#30)

--- packages/zip-stream/src/compress-commons.ts
+++ packages/zip-stream/src/compress-commons.ts
@@ -1,15 +1,7 @@
-import { ArchiveEntry } from "./archivers/archive-entry";
-import { ArchiveOutputStream } from "./archivers/archive-output-stream";
 import { ZipArchiveEntry } from "./archivers/zip-archive-entry";
 import {
   ZipArchiveOutputStream,
   type ZipOptions,
 } from "./archivers/zip-archive-output-stream";
 
-export {
-  ArchiveEntry,
-  ArchiveOutputStream,
-  ZipArchiveEntry,
-  ZipArchiveOutputStream,
-  type ZipOptions,
-};
+export { ZipArchiveEntry, ZipArchiveOutputStream, type ZipOptions };
